040492d0c603d1f8523ad07f864160547c8b9f8f
hrk fkm <fkm@mokelab.com>
2017-01-14 02:10:11 +0900

    Implementation

--- impl/client.go
+++ impl/client.go
@@ -8,6 +8,10 @@ import (
 	"net/http"
 )
 
+const (
+	fcm_url = "https://fcm.googleapis.com/fcm/send"
+)
+
 type client struct {
 	apiKey string
 }
@@ -18,6 +22,27 @@ func NewClient(apiKey string) *client {
 	}
 }
 
-func (c *client) Send(notification Notification, data Data, regIds ...string) (fcm.Response, error) {
+func (c *client) Send(notification fcm.Notification, data fcm.Data, regIds ...string) (fcm.Response, error) {
+	msg := map[string]interface{}{
+		"registration_ids": regIds,
+		"data":             data,
+	}
+	body, err := json.Marshal(msg)
+	if err != nil {
+		return fcm.Response{}, err
+	}
+	req, err := http.NewRequest("POST", fcm_url, bytes.NewBuffer(body))
+	if err != nil {
+		return fcm.Response{}, err
+	}
+	req.Header.Add("Authorization", fmt.Sprintf("key=%s", c.apiKey))
+	req.Header.Add("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return fcm.Response{}, err
+	}
+	defer resp.Body.Close()
+
 	return fcm.Response{}, nil
 }
---
+++ impl/client_test.go
@@ -0,0 +1,21 @@
+package impl
+
+import (
+	"github.com/mokelab-go/fcm"
+	"testing"
+)
+
+const (
+	test_api_key = "INPUT_YOUR_API_KEY"
+	test_reg_id  = "INPUT_YOUR_REG_ID"
+)
+
+func Test_Send(t *testing.T) {
+	client := NewClient(test_api_key)
+	notification := fcm.Notification{}
+	data := fcm.Data{}
+	_, err := client.Send(notification, data, test_reg_id)
+	if err != nil {
+		t.Errorf("Failed to send FCM message : %s", err)
+	}
+}
